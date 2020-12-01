7b0aa58a65b44510c92176fe42c03352ca7c1b19
adnan morshed <adnan.starit118@gmail.com>
2020-11-30 17:44:39 -0800

    item bloc event class

---
+++ lib/bloc patten/itemblocEvent/ItemBlocEvent.dart
@@ -0,0 +1,22 @@
+
+
+abstract class ItemBlocEvent {}
+
+class AddToCart extends ItemBlocEvent{
+
+}
+class AddToCartRemove extends ItemBlocEvent{
+
+}
+
+class AddToCartTotalAmount extends ItemBlocEvent{
+
+}
+
+class AddToCartShiipingAmount extends ItemBlocEvent{
+
+}
+
+class AddToCartFinalAmount extends ItemBlocEvent{
+
+}
